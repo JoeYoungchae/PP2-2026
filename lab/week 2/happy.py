470b3d014d30c91898685f96592de445336dbcae
Joe Young-chae <choyc0123@gmail.com>
2026-09-09 13:48:55 +0900

    생일 축하 함수 및 테스트함수 작성

---
+++ lab/week 2/happy.py
@@ -0,0 +1,28 @@
+#
+# 생일 축하 함수
+#
+def say_happy_birthday(name:str) -> None:
+    print("안녕하세요")
+    print(name + "님의 생일을 축하합니다")
+    return None
+
+def test_happy_birthday() :
+    say_happy_birthday("영채")
+    say_happy_birthday("윤빈")
+    say_happy_birthday("민솔")
+    say_happy_birthday("지연")
+
+def test_happy_birthday2() :
+    names = ["영채", "윤빈", "민솔", "지연"]
+    for name in names:
+        say_happy_birthday(name)
+
+def test_happy_birthday3() :
+    say_happy_birthday(3.14)
+    say_happy_birthday([1,2,3])
+
+if __name__ == "__main__":
+#    test_happy_birthday()
+#    test_happy_birthday2()
+    test_happy_birthday3()
+
